0d279b5061de436d9d02dffb620e61918c078f59
FabAgent26 <fab.gestion25@gmail.com>
2026-08-05 19:19:42 +0300

    Retour a l'accueil a la reouverture de l'app + titre cliquable

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker - rend l'appli disponible hors connexion.
    Strategie: cache-first pour l'app shell, avec mise a jour en arriere-plan. */
-const CACHE = "muscu-fanny-v10";
+const CACHE = "muscu-fanny-v11";
 const ASSETS = [
   "./",
   "./index.html",
