825d11cdf66a65a8fa05316b05d03b6d8a3f32e4
FabAgent26 <fab.gestion25@gmail.com>
2026-08-05 20:10:38 +0300

    Noms de seances importees (Full body A/B) + version 1.4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker - rend l'appli disponible hors connexion.
    Strategie: cache-first pour l'app shell, avec mise a jour en arriere-plan. */
-const CACHE = "muscu-fanny-v14";
+const CACHE = "muscu-fanny-v15";
 const ASSETS = [
   "./",
   "./index.html",
